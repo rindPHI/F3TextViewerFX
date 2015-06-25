172dca57d3caf343a8631378c24036e8e687f0d5
Dominic Scheurer <scheurer@cs.tu-darmstadt.de>
2015-06-25 09:38:09 +0200

    Added license information§

--- src/de/dominicscheurer/quicktxtview/view/MaxFileSizeSettingsDialogController.java
+++ src/de/dominicscheurer/quicktxtview/view/MaxFileSizeSettingsDialogController.java
@@ -1,3 +1,21 @@
+/* This file is part of F3TextViewerFX.
+ * 
+ * F3TextViewerFX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * F3TextViewerFX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with F3TextViewerFX.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright 2015 by Dominic Scheurer <dscheurer@dominic-scheurer.de>.
+ */
+
 package de.dominicscheurer.quicktxtview.view;
 
 import de.dominicscheurer.quicktxtview.model.FileSize;
